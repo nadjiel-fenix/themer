1761d4a2a8a9d4c5c10dea5c2b2fa14cd7388a59
nadjiel-fenix <daniel@fenixtecnologia.info>
2025-07-21 10:15:44 -0300

    :sparkles: feat (themes): created dark theme

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   palette: {
-    mode: "light",
+    mode: "dark",
     primary: { main: "#4169E1" },
     secondary: { main: "#E91E63" },
   },
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,1 +1,2 @@
 export { default as light } from "./light";
+export { default as dark } from "./dark";
