c6db30c449e52652750f7a26ddf0f02665ecba17
nadjiel-fenix <daniel@fenixtecnologia.info>
2025-07-21 10:04:16 -0300

    :sparkles: feat (themes): created dedicated module

---
+++ src/themes/light.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "light", // or 'dark'
+    primary: { main: "#4169E1" },
+    secondary: { main: "#E91E63" },
+  },
+});
+
+export default theme;
